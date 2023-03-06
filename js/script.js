351586966c2b5d762cb7933c03e281cd4db27c88
Xerlok <20791727+Xerlok@users.noreply.github.com>
2023-03-06 15:26:55 +0300

    Add grid generator with draw and grid size change

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
 const container = document.querySelector('#grid-container');
+const btns = document.querySelectorAll('.grid-size');
+
 
 function start(){
-    makeGrid(32);
-    draw();
+    makeGrid(16);
+    changeGridSize();
 }
 
 function makeGrid(gridSize) {
     container.style.setProperty('--grid-rows', gridSize);
     container.style.setProperty('--grid-cols', gridSize);
     for (i = 0; i < (gridSize * gridSize); i++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-  };
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
+
+    draw();
 }
 
 function draw() {
     const grids = document.querySelectorAll('.grid-item');
     grids.forEach((grid) => {grid.addEventListener('mouseover', () => {
         grid.style.backgroundColor = 'black';
+    });
+    });
+}
+
+function eraseAll() {
+    const grids = document.querySelectorAll('.grid-item');
+    grids.forEach((grid) => {grid.remove()});
+
+}
+
+function changeGridSize() {
+    btns.forEach((btn) => {btn.addEventListener('click', () => {
+        gridSize = parseInt(btn.id);
+        eraseAll();
+        makeGrid(gridSize);
     });
     });
 }
